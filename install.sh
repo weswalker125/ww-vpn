3ba3fc3b124cb734e6e3b770295665634f7b6770
Wes <weswalker125@gmail.com>
2018-02-03 21:50:45 -0500

    correction to cert generation calls (other way made only clients somehow... was an issue. --remote-cert-tls must be specified - it's denoting what it expects for the type of cert on the other side (server or client, for TLS Web Server Authentication or TLS Web Client Authentication respectively).

--- install.sh
+++ install.sh
@@ -9,32 +9,34 @@ modprobe iptable_nat
 echo 1 | tee /proc/sys/net/ipv4/ip_forward
 iptables -t nat -A POSTROUTING -s 10.4.0.1/2 -o eth0 -j MASQUERADE
 iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
+iptables-save
 cd /usr/share/easy-rsa/3
 ./easyrsa init-pki
 # Create CA at ./pki/ca.crt
 echo "ww-vpn-ca" | ./easyrsa build-ca nopass
-# Create diffie-hellman parameters at ./pki/dh.pem
-./easyrsa gen-dh
-# Create server cert at ./pki/issued/ww-vpn.crt, server key at ./pki/private/ww-vpn.key
-./easyrsa build-server-full ww-vpn nopass
+# Create server cert at ./pki/issued/*.crt, server key at ./pki/private/*.key
+./easyrsa build-server-full server nopass
 # Create client certs at ./pki/issued/*.crt, server key at ./pki/private/*.key
 CLIENTS=(device-laptop device-tablet device-phone)
 for client in "${CLIENTS[@]}"; do
   ./easyrsa build-client-full $client nopass
 done
+# Create diffie-hellman parameters at ./pki/dh.pem
+./easyrsa gen-dh
 openvpn --genkey --secret ./pki/private/pfs.key
 mkdir -p /etc/openvpn/keys
 rm ./pki/private/ca.key
 find . -type f -name "*.crt" -exec cp {} /etc/openvpn/keys/ \;
 find . -type f -name "*.key" -exec cp {} /etc/openvpn/keys/ \;
+find . -type f -name "*.pem" -exec cp {} /etc/openvpn/keys/ \;
 
 aws s3 cp s3://${ConfigBucket}/ww-vpn/openvpn-server.conf /etc/openvpn/server.conf
 # Replace placeholders with content
 sed -i -e '/%PFS_KEY%/{r ./pki/private/pfs.key' -e 'd}' /etc/openvpn/server.conf
 sed -i -e '/%DH_PEM%/{r ./pki/dh.pem' -e 'd}' /etc/openvpn/server.conf
 sed -i -e '/%CA_CRT%/{r ./pki/ca.crt' -e 'd}' /etc/openvpn/server.conf
-sed -i -e '/%CRT%/{r ./pki/issued/ww-vpn.crt' -e 'd}' /etc/openvpn/server.conf
-sed -i -e '/%KEY%/{r ./pki/private/ww-vpn.key' -e 'd}' /etc/openvpn/server.conf
+sed -i -e '/%CRT%/{r ./pki/issued/server.crt' -e 'd}' /etc/openvpn/server.conf
+sed -i -e '/%KEY%/{r ./pki/private/server.key' -e 'd}' /etc/openvpn/server.conf
 
 # Create client configs
 mkdir -p /home/ec2-user/ww-vpn
@@ -47,8 +49,8 @@ for client in "${CLIENTS[@]}"; do
   sed -i -e '/%DH_PEM%/{r ./pki/dh.pem' -e 'd}' $FILE
   sed -i -e '/%CA_CRT%/{r ./pki/ca.crt' -e 'd}' $FILE
   #didn't work:
-  mv ./pki/issued/${client}.crt ./pki/issued/current.crt
-  mv ./pki/private/${client}.key ./pki/private/current.key
+  cp ./pki/issued/${client}.crt ./pki/issued/current.crt
+  cp ./pki/private/${client}.key ./pki/private/current.key
   sed -i -e '/%CRT%/{r ./pki/issued/current.crt' -e 'd}' $FILE
   sed -i -e '/%KEY%/{r ./pki/private/current.key' -e 'd}' $FILE
 done
